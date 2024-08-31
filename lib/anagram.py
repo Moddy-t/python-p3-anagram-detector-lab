9fd2f0294612e09334240b96ea2b9466a86036c8
Moddy-t <amalembamodesta@gmail.com>
2024-08-31 12:36:51 +0300

    anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+    
+    def match(self, word_list):
+        result = []
+        for w in word_list:
+            if sorted(w) == self.sorted_word:
+                result.append(w)
+        return result
